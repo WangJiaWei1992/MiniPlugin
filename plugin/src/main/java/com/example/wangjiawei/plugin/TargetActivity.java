4c558e80280e3604a281d50a6bb7f0d478df02b7
wangjiawei3 <wangjiawei3@kingsoft.com>
2017-02-14 17:38:03 +0800

    Clean Code

--- plugin/src/main/java/com/example/wangjiawei/plugin/TargetActivity.java
+++ plugin/src/main/java/com/example/wangjiawei/plugin/TargetActivity.java
@@ -3,12 +3,17 @@ package com.example.wangjiawei.plugin;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class TargetActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_target);
+        //setContentView(R.layout.activity_target);
+        TextView textView = new TextView(this);
+        textView.setText("Target");
+        setContentView(textView);
     }
 }
